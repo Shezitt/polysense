6d6ca61982b8523674202f330c527ba068cafede
znolly <173570505+znolly@users.noreply.github.com>
2025-12-14 19:41:35 -0400

    some fixes

--- resources/views/modulo3.blade.php
+++ resources/views/modulo3.blade.php
@@ -68,7 +68,7 @@
                     </div>
 
                     <div class="pt-2">
-                        <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors">
+                        <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-colors">
                             Ejecutar Limpieza
                         </button>
                     </div>
