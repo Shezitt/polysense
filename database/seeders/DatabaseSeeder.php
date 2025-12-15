9681abacc43e65e7e397386d4cb33b06b630825f
Shamir <shamirteranmustafa@gmail.com>
2025-12-14 20:53:09 -0400

    add seeder para comandos de voz

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Cargar comandos de voz predeterminados
+        $this->call([
+            VoiceCommandSeeder::class,
+        ]);
     }
 }
