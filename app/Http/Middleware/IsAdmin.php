c5353a32b060c7277e18a6ce98305d9301febaba
Shamir <shamirteranmustafa@gmail.com>
2025-12-16 12:28:44 -0400

    limpieza de middleware

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -8,19 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        // Verificar si el usuario está autenticado
         if (!auth()->check()) {
             return redirect()->route('login')->with('error', 'Debes iniciar sesión');
         }
 
-        // Verificar si el usuario es administrador
         if (auth()->user()->role !== 'admin') {
             abort(403, 'No tienes permisos para acceder a esta sección');
         }
